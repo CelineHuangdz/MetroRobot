11df197f6bce9dd4a705c7350962b476e8c1002b
CelineHuangdz <1056148466@qq.com>
2020-12-06 11:52:02 +0800

    主页面智能客服基本排版完成，添加了底部导航栏。有些细节有待完善

--- miniprogram/pages/MainPage-Robot/robot.js
+++ miniprogram/pages/MainPage-Robot/robot.js
@@ -1,18 +1,49 @@
 // miniprogram/pages/MainPage-Robot/robot.js
+const app = getApp();
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+function initData(that) {
+  inputVal = '';
+
+  msgList = [{
+      speaker: 'server',
+      contentType: 'text',
+      content: '欢迎来到地铁智能客服！'
+    },
+    {
+      speaker: 'customer',
+      contentType: 'text',
+      content: '你好'
+    }
+  ]
+  that.setData({
+    msgList,
+    inputVal
+  })
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    scrollHeight: '100vh',
+    inputBottom: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    initData(this);
+    this.setData({
+      cusHeadIcon: app.globalData.userInfo.avatarUrl,
+    });
   },
 
   /**
@@ -62,5 +93,51 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  focus: function(e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
+    })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
+
+  },
+
+  //失去聚焦(软键盘消失)
+  blur: function(e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 0
+    })
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
+    })
+
+  },
+
+  /**
+   * 发送点击监听
+   */
+  sendClick: function(e) {
+    msgList.push({
+      speaker: 'customer',
+      contentType: 'text',
+      content: e.detail.value
+    })
+    inputVal = '';
+    this.setData({
+      msgList,
+      inputVal
+    });
+  },
+
+  toBackClick: function() {
+    wx.navigateBack({})
   }
 })
